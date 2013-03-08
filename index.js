bb5125d879720b26f8386a8fdc103bee5052d639
Christopher <c.freden@gmail.com>
2013-03-08 08:55:49 +0100

    Added simple tinci install function to tinci vision

--- index.js
+++ index.js
@@ -4,9 +4,22 @@ var fs = require('fs'),
     path = require('path'),
     parse = require('url').parse,
     http = require('http'),
+    exec = require('child_process').exec,
     rootpath = path.resolve(process.argv[2] || '.'),
     port = parseInt(process.argv[3]) || 4567,
-    tmpl = fs.readFileSync(require.resolve('./template.html'), 'utf8');
+    tmpl = fs.readFileSync(require.resolve('./template.html'), 'utf8'),
+    hookpath = require.resolve('./hooks/tinci');
+
+function copyHook(to, runner, branch, callback) {
+  var cmd = 'cp '+hookpath+' '+to+'/hooks/post-receive && git config -f '+
+    to+'/config --add tinci.runner "'+runner+'" && git config -f ' +
+    to+'/config --add tinci.branch "'+branch+'" && mkdir '+to+'/.tinci';
+  console.log(cmd);
+  exec(cmd, function (err, stdout, stderr) {
+    console.log(err, stdout, stderr);
+    callback(err, stdout, stderr);
+  });
+}
 
 function template(model) {
   var html = tmpl, i;
@@ -28,31 +41,49 @@ function logToHtml(file) {
   };
 }
 
+function logHtml(tincipath) {
+  return fs.readdirSync(tincipath).filter(function(file){
+    return file.slice(-4) === '.log';
+  })
+  .map(function(file) {
+    return logToHtml(path.join(tincipath, file));
+  })
+  .sort(function(a, b){return b.ctime-a.ctime})
+  .map(function(log){return log.html;})
+  .join('');
+}
+
 http.createServer(function(req, res) {
-  var pathname = path.resolve(rootpath, './'+parse(req.url).pathname),
-      tincipath, model = {};
+  var url = parse(req.url, true),
+      pathname = path.resolve(rootpath, './'+url.pathname),
+      reponame, tincipath, model = {};
   if (pathname.indexOf(rootpath) === 0 && fs.existsSync(pathname)) {
+    reponame = path.basename(pathname);
     if (fs.existsSync(path.join(pathname, '.git')))
       pathname = path.join(pathname, '.git');
-    tincipath = path.join(pathname, '.tinci');
-    if (fs.existsSync(tincipath)) {
+    if (fs.existsSync(path.join(pathname, 'hooks'))) {
       res.writeHead(200, {
         'Content-Type': 'text/html'
       });
-      model.title = path.basename(pathname);
-      model.logs = fs.readdirSync(tincipath).filter(function(file){
-        return file.slice(-4) === '.log';
-      })
-      .map(function(file) {
-        return logToHtml(path.join(tincipath, file));
-      })
-      .sort(function(a, b){return b.ctime-a.ctime})
-      .map(function(log){return log.html;})
-      .join('');
-      res.end(template(model));
+      model.title = reponame;
+      tincipath = path.join(pathname, '.tinci');
+      if (url.query.runner) {
+        copyHook(pathname, url.query.runner, url.query.branch||'master', function(err) {
+          model.logs = "No logs yet";
+          res.end(template(model));
+        });
+      } else {
+        if (fs.existsSync(tincipath)) {
+          model.logs = logHtml(tincipath);
+        } else {
+          model.config = 'show';
+          model.logs = '';
+        }
+        res.end(template(model));
+      }
     } else {
       res.writeHead(500);
-      res.end("tinci not installed in git repo "+pathname+"?");
+      res.end("Directory not a git repo?");
     }
   } else {
     res.writeHead(404);
